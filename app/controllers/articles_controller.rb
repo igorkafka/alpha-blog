0d4612e88ff356e5d08b5d03bf31f7cac86ddda4
YOUR NAME <igor_memories@hotmail.com>
2020-11-10 14:55:32 -0400

    Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,21 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfly."
+            redirect_to article_path(@article)
+        else 
+            render 'new'
+        end
+    end
+
+    
+
     
 end
