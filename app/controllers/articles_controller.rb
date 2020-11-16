0ab82f993878084c1580a0c8a4c5764bc9ad23e2
YOUR NAME <igor_memories@hotmail.com>
2020-11-16 07:11:11 -0400

    Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destory]
     def show
-        @article = Article.find(params[:id])
     end
     
     def index
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfly."
             redirect_to article_path(@article)
@@ -21,6 +21,33 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was edited"
+            redirect_to @article
+        else 
+            debugger
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+    
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
     
 
     
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,8 @@
 require_relative 'application'
 
 # Initialize the Rails application.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end 
+
 Rails.application.initialize!
