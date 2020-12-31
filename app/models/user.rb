ce63b9c2bc546dabc881f433a9fbfa00a9828e4f
YOUR NAME <igor_memories@hotmail.com>
2020-12-31 06:32:32 -0400

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
+    has_many :articles
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3 , maximum: 25}, format: { with: VALID_EMAIL_REGEX }
     
 
     has_many :articles
+    has_secure_password
+
     
 end
